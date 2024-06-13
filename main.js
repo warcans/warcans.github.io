756e39d8cdeeb97b4bdfd04c28275b8dcbb19db9
warcans <kanajalka@gmail.com>
2024-06-13 15:02:38 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-const streamerFetch = await fetch("streamers.json")
-const streamers = await streamerFetch.json()
+var streamerFetch = await fetch("streamers.json")
+var streamers = await streamerFetch.json()
+
+// const url = 'https://gist.githubusercontent.com/warcans/be6d1af29fba88ee1d458feff9bb7641/raw/live_status.json'
+
+var live_statusFetch = await fetch("live_status.json")
+// loadXMLDoc()
+
+var live_status = await live_statusFetch.json() 
 
 const template = document.querySelector("#default-card")
+
 const wrapper = document.createDocumentFragment("div")
 
 const fileExt = [".jpg", ".png"]
@@ -39,7 +47,38 @@ streamers.forEach(user => {
 
     clone.querySelector("#bgcolor").style = "background: linear-gradient(120deg, #202020 , " + user.color1 + ");"
 
-    wrapper.appendChild(clone)
-});
+    // SET LIVE STATUS
+    clone.querySelector("#live-status").textContent = "OFFLINE"
+    live_status.forEach(key =>{
+        if (key.user_name.toLowerCase() == user.name.toLowerCase()){
+            clone.querySelector("#live-status").textContent = "LIVE"
+            if (key.is_minecraft == "true" && key.is_amigis == "true"){
+                clone.querySelector("#live-status").textContent = "AMIGI"
+            }
+        }
+    })
+    wrapper.appendChild(clone);
+})
 
 document.querySelector(".streamers").appendChild(wrapper)
+
+// function loadXMLDoc() {
+//     var xmlhttp = new XMLHttpRequest();
+
+//     xmlhttp.onreadystatechange = function() {
+//         if (xmlhttp.readyState == XMLHttpRequest.DONE) { // XMLHttpRequest.DONE == 4
+//            if (xmlhttp.status == 200) {
+//                 live_statusFetch = xmlhttp.responseText;
+//            }
+//            else if (xmlhttp.status == 400) {
+//               alert('There was an error 400');
+//            }
+//            else {
+//                alert('something else other than 200 was returned');
+//            }
+//         }
+//     };
+
+//     xmlhttp.open("GET", url, true);
+//     xmlhttp.send();
+// }
