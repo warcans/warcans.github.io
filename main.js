494cfef67b23b1965c7ae25c96d04290f2b33c36
warcans <kanajalka@gmail.com>
2024-06-17 02:30:15 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,76 +1,89 @@
 var streamerFetch = await fetch("streamers.json")
 var streamers = await streamerFetch.json()
 
-// LIVE PUBLISH
-const url = 'https://gist.githubusercontent.com/warcans/be6d1af29fba88ee1d458feff9bb7641/raw/live_status.json'
-var live_statusFetch
-loadXMLDoc()
-var live_status = JSON.parse(live_statusFetch)
+//// LIVE VERSION
+//// URL needs to be the URL to which backend serves data
+// const url = 'https://gist.githubusercontent.com/warcans/be6d1af29fba88ee1d458feff9bb7641/raw/live_status.json'
+// var live_statusFetch
+// loadXMLDoc()
+// var live_status = JSON.parse(live_statusFetch)
 
-// DEV
-// var live_statusFetch = await fetch("live_status.json")
-// var live_status = await live_statusFetch.json()
+//// DEV VERSION
+var live_statusFetch = await fetch("live_status.json")
+var live_status = await live_statusFetch.json()
 
+// This is the card template
 const template = document.querySelector("#default-card")
+const social_template = document.querySelector("#social-link")
 
+// Wrappers for each status. Used for filtering
 const wrapper_all = document.createDocumentFragment("div")
 const wrapper_live = document.createDocumentFragment("div")
 const wrapper_amigi = document.createDocumentFragment("div")
 
-const fileExt = [".jpg", ".png"]
+// Short for social_channels
+const social_channels = streamers.social_channels
 
-streamers.forEach(user => {
+streamers.streamers.forEach(user => {
     const clone = template.content.cloneNode(true)
 
-    var filter = true
-    var live = false
-    var amigi = false
+    var filter = true   // Sets cards in order of: AMIGI > LIVE > OFFLINE
+    var live = false    // Gets true if user is live
+    var amigi = false   // Gets true if user is live, playing minecraft and includes "amigis" in title
 
+    // Set user photo
     const img = clone.querySelector("#photo")
     img.src = user.image
 
+    // Set twitch link to card and photo
     const links = clone.querySelectorAll("#card-link")
-    links.forEach(e =>{
-        e.href = "https://twitch.tv/" + user.name
-    })
+    links.forEach(e => { e.href = "https://twitch.tv/" + user.name })
 
+    // Set username and description
     clone.querySelector("#username").textContent = user.name
-
     clone.querySelector("#description").textContent = user.description
 
-    var twitter = clone.querySelector("#twitter")
-    var bluesky = clone.querySelector("#bluesky")
-    var youtube = clone.querySelector("#youtube")
-    var tiktok = clone.querySelector("#tiktok")
-    var discord = clone.querySelector("#discord")
+    // Get social media elements
+    const social_container = clone.querySelector(".card-links")
+    const social_wrapper = document.createDocumentFragment("div")
 
-    if (user.twitter != null) twitter.href = "https://twitter.com/" + user.twitter
-    else twitter.remove()
+    // Add a social media link for each mentioned for user in streamers.json
+    for (var social in user.socials){
 
-    if (user.bluesky != null) bluesky.href = "https://bsky.app/profile/" + user.bluesky
-    else bluesky.remove()
+        // Copy template
+        const  user_social = social_template.content.cloneNode(true)
+        var service
+        var icon
 
-    if (user.youtube != null) {
-        if (user.youtube.includes("@")) youtube.href = "https://youtube.com/" + user.youtube
-        else youtube.href = "https://youtube.com/channel/" + user.youtube
+        // Loop through configured socials in .json to find the correct data
+        social_channels.forEach(channel =>{
+            if (channel.name == social){
+                service = channel.prefix
+                icon = channel.icon
+            }
+        })
+
+        // Set link and icon
+        user_social.querySelector("#social").href = "" + service + user
+        user_social.querySelector("#social-icon").src = icon
+
+        // Add to template
+        social_wrapper.appendChild(user_social)
     }
-    else youtube.remove()
 
-    if (user.tiktok != null) tiktok.href = "https://tiktok.com/" + user.tiktok
-    else tiktok.remove()
+    // Apply template to card
+    social_container.appendChild(social_wrapper)
 
-    if (user.discord != null) discord.href = "https://discord.com/" + user.discord
-    else discord.remove()
-
+    // Set card background to user color
     var card_style = "background: " + user.color1 + ";"
-    // var card_style = "background: linear-gradient(180deg, #404040 25%, " + user.color1 + " 75%);"
-    // SET LIVE STATUS
+
+    // Set live status and configure card effects
     var card_status = clone.querySelector("#live-status")
     card_status.textContent = "OFFLINE"
 
     live_status.forEach(key =>{
 
-        // SET ELEMENTS FOR LIVES
+        // if user is live
         if (key.user_name.toLowerCase() == user.name.toLowerCase()){
             live = true
             var animation_length = ((100/key.game.length*key.game.length)/600) / (1/(key.game.length+26))
@@ -80,7 +93,7 @@ streamers.forEach(user => {
             var a = clone.getElementById("bgcolor")
             a.setAttribute("status", "live")
 
-            // SET ELEMENTS FOR AMIGI LIVES
+            // if user is playing minecraft and has amigi in title
             if (key.is_minecraft == "true" && key.is_amigis == "true"){
                 amigi = true
                 var amigi_text = "AMIGI MINECRAFT"
@@ -93,10 +106,13 @@ streamers.forEach(user => {
         }
     })
 
+    // Set card effects
     clone.querySelector("#bgcolor").style = card_style
 
+    // Set static font for offline users
     if (live == false) clone.querySelector(".card-live-status").style = "display:flex;justify-content:center;"
 
+    // Add users in filtering order
     if (filter){
         if (amigi == true){
             wrapper_amigi.appendChild(clone)
@@ -106,9 +122,18 @@ streamers.forEach(user => {
         }
     }
 
+    // Add non-filtered users to all
     wrapper_all.appendChild(clone);
 })
 
+// Add cards to website
+document.querySelector(".streamers").appendChild(wrapper_amigi)
+document.querySelector(".streamers").appendChild(wrapper_live)
+document.querySelector(".streamers").appendChild(wrapper_all)
+
+// Fetch live data from GIST
+// Backend is completely separate from anything and hosted in Oracle Cloud
+// It uses the users from streamers.json to fetch data from twitch API
 function loadXMLDoc() {
     var xmlhttp = new XMLHttpRequest()
 
@@ -129,7 +154,3 @@ function loadXMLDoc() {
     xmlhttp.open("GET", url, false)
     xmlhttp.send()
 }
-
-document.querySelector(".streamers").appendChild(wrapper_amigi)
-document.querySelector(".streamers").appendChild(wrapper_live)
-document.querySelector(".streamers").appendChild(wrapper_all)
